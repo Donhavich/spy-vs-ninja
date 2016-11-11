dbb8809021c07f67c21d411030b92acc244abe9e
Donhavich <gonzalezdodbt@gmail.com>
2016-11-11 15:42:27 -0800

    Update Room.java

--- CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Room.java
+++ CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Room.java
@@ -9,9 +9,17 @@ package edu.cpp.cs.cs141.final_project;
  */
 public class Room implements SquareObject {
 	
+	private int x;
+	private int y;
+	private boolean die;
+	
 	public Room(){};
 	
 	public boolean file(){};
+	public void setX(int X){}
+	public void setY(int Y){}
+	public int getX(){}
+	public int getY(){}
 	
 	
 }
