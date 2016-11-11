a9474e2c56a0e0d923dbade031d1a2d600c45033
Donhavich <gonzalezdodbt@gmail.com>
2016-11-11 15:52:28 -0800

    Update Grid.java

--- CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Grid.java
+++ CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Grid.java
@@ -15,12 +15,9 @@ public class Grid {
   }
   public String ToString(boolean isDebug){
       String gridDisplay;
-      for(int r = 0; r < grid.length;r++)
-      {
-        for(int c = 0; c < grid[0].length; c++)
-        {
-          if(grid[r][c] == null)
-          {
+      for(int r = 0; r < grid.length;r++){
+        for(int c = 0; c < grid[0].length; c++){
+          if(grid[r][c] == null){
             if(isDebug)
               gridDisplay += "[ ]";
             else
@@ -31,54 +28,26 @@ public class Grid {
         }
         gridDisplay += "\n";
       }
-	}
+  }
   
-  public void setPlayerCoord(){
-     for(int r = 0; r < grid.length;r++)
-      {
-        for(int c = 0; c < grid[0].length; c++)
-        {
-          if(Floor[r][c] instanceof Spy){
+  private void setEntityCoord(){
+     for(int r = 0; r < grid.length;r++){
+        for(int c = 0; c < grid[0].length; c++){
+          if(Floor[r][c] instanceof SquareObject){
             Floor[r][c].setX(c);
             Floor[r][c].setY(r);
           }
         }
-      }
-  }
-    public void setNinjaCoord(){
-     for(int r = 0; r < grid.length;r++)
-      {
-        for(int c = 0; c < grid[0].length; c++)
-        {
-          if(Floor[r][c] instanceof Ninja){
-            Floor[r][c].setX(c);
-            Floor[r][c].setY(r);
-          }
-        }
-      }
-  }
-    public void setRoomCoord(){
-     for(int r = 0; r < grid.length;r++)
-      {
-        for(int c = 0; c < grid[0].length; c++)
-        {
-          if(Floor[r][c] instanceof Room){
-            Floor[r][c].setX(c);
-            Floor[r][c].setY(r);
-          }
-        }
-      }
-  }
-    public void setItemCoord(){
-     for(int r = 0; r < grid.length;r++)
-      {
-        for(int c = 0; c < grid[0].length; c++)
-        {
-          if(Floor[r][c] instanceof Item){
-            Floor[r][c].setX(c);
-            Floor[r][c].setY(r);
-          }
-        }
-      }
+     }
   }
 }
+ private void setRooms(Room [] rooms){
+	 int i = 0;
+ 	for(int r = 1: r < Floor.length; r+=3){
+		for(int c = 1; Floor[0].length; c+=3){
+			Floor[r][c] = rooms[i];
+			i++;
+		}
+	}
+ }
+ }
