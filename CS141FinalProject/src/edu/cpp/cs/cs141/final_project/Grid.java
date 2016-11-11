69b97b05a7284e77e4cfe65f6d22a6bfbe71d1f3
Donhavich <gonzalezdodbt@gmail.com>
2016-11-10 19:24:51 -0800

    Update Grid.java

--- CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Grid.java
+++ CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Grid.java
@@ -8,5 +8,77 @@ package edu.cpp.cs.cs141.final_project;
  *
  */
 public class Grid {
-
+  private SquareObjects [][] Floor = new SquareObjects [9][9];
+  
+  public Grid(Spy player, Ninja [] ninjas, Room [] rooms, Item [] items){
+    
+  }
+  public String ToString(boolean isDebug){
+      String gridDisplay;
+      for(int r = 0; r < grid.length;r++)
+      {
+        for(int c = 0; c < grid[0].length; c++)
+        {
+          if(grid[r][c] == null)
+          {
+            if(isDebug)
+              gridDisplay += "[ ]";
+            else
+              gridDisplay += "[*]";
+          }
+          else
+            gridDisplay += grid[r][c].ToString(isDebug);
+        }
+        gridDisplay += "\n";
+      }
+	}
+  
+  public void setPlayerCoord(){
+     for(int r = 0; r < grid.length;r++)
+      {
+        for(int c = 0; c < grid[0].length; c++)
+        {
+          if(Floor[r][c] instanceof Spy){
+            Floor[r][c].setX(c);
+            Floor[r][c].setY(r);
+          }
+        }
+      }
+  }
+    public void setNinjaCoord(){
+     for(int r = 0; r < grid.length;r++)
+      {
+        for(int c = 0; c < grid[0].length; c++)
+        {
+          if(Floor[r][c] instanceof Ninja){
+            Floor[r][c].setX(c);
+            Floor[r][c].setY(r);
+          }
+        }
+      }
+  }
+    public void setRoomCoord(){
+     for(int r = 0; r < grid.length;r++)
+      {
+        for(int c = 0; c < grid[0].length; c++)
+        {
+          if(Floor[r][c] instanceof Room){
+            Floor[r][c].setX(c);
+            Floor[r][c].setY(r);
+          }
+        }
+      }
+  }
+    public void setItemCoord(){
+     for(int r = 0; r < grid.length;r++)
+      {
+        for(int c = 0; c < grid[0].length; c++)
+        {
+          if(Floor[r][c] instanceof Item){
+            Floor[r][c].setX(c);
+            Floor[r][c].setY(r);
+          }
+        }
+      }
+  }
 }
