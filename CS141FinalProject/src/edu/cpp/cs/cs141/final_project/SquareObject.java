66b450df21165f5a2bb2ba86f981dc08dc010d7c
Hikaru5 <hikarukun5@gmail.com>
2016-11-13 15:02:30 -0800

    Update SquareObject.java

--- CS141FinalProject/src/edu/cpp/cs/cs141/final_project/SquareObject.java
+++ CS141FinalProject/src/edu/cpp/cs/cs141/final_project/SquareObject.java
@@ -8,5 +8,14 @@ package edu.cpp.cs.cs141.final_project;
  *
  */
 public interface SquareObject {
-
+  
+  /*
+  * Represents the object's horizontal location on the grid.
+  */
+  private int x;
+  
+  /**
+  * Respresents the object's vertical location on the grid.
+  */
+  private int y;
 }
