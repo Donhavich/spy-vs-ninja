d4accc01b6ed4bc58f97fba2ab89948a6188800f
Hikaru5 <hikarukun5@gmail.com>
2016-11-13 15:07:04 -0800

    Update SquareObject.java

--- CS141FinalProject/src/edu/cpp/cs/cs141/final_project/SquareObject.java
+++ CS141FinalProject/src/edu/cpp/cs/cs141/final_project/SquareObject.java
@@ -8,5 +8,16 @@ package edu.cpp.cs.cs141.final_project;
  *
  */
 public interface SquareObject {
+
+  /**
+  * Assigns the location of the objects on the grid.
+  */
+  setX();
+  setY();
   
+  /**
+  * Returns the individual x and y coordinates of the object.
+  */
+  getX();
+  getY();
 }
