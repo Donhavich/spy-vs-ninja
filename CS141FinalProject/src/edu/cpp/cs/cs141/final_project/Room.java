cc1d998eb8b536c1c9ed42ea60f08d929ec2fb99
lily21 <lynnnguyenquach@gmail.com>
2016-11-15 03:38:10 -0800

    Update Room.java

--- CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Room.java
+++ CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Room.java
@@ -21,29 +21,60 @@ package edu.cpp.cs.cs141.final_project;
  * The class Room extends the {@link SquareObject} class. There are nine of {@link Room} printed
  * and equally distributed across the {@link Grid} but only one {@link Room} holds the briefcase
  * of documents that the {@link Spy} must retrieve to win the game. 
+ *
  * @author
  *
  */
 public class Room extends SquareObject {
 	
 	/**
-	 * 
+	 * This field represents whether {@link Room} holds the case. If {@link Room} does not
+	 * contain the briefcase, {@link #hasCase} is {@code false}. Otherwise, if {@link Room}
+	 * does contain the briefcase, {@link #hasCase} is true. Only one {@link Room} will
+	 * have the briefcase for {@link Spy} to find.
 	 */ 
-
 	private boolean hasCase=false;
 	
+	/**
+	 * This is the default constructor for the class {@link Room}. This initializes the
+	 * {@link Room} class.
+	 */
 	public Room(){};
 	
+	/**
+	 * This method returns whether {@link Room} contains the briefcase to {@link Spy}.
+	 * @return when {@link Spy} enters {@link Room} and if {@link Room} has the
+	 * 	   briefcase, then {@link #hasCase} is {@code true}. If not, then
+	 *	   {@link #hasCase} is {@code false}.
+	 */
 	public boolean hasCase()
 	{
 		return hasCase;
 	}
 	
+	/**
+	 * This method gives the briefcase if {@link Spy} enters the {@link Room}
+	 * with the briefcase. If {@link Room} has the briefcase, then
+	 * {@link #hasCase} is true and it is given to {@link Spy] who wins the game.
+	 * If not, then {@link #hasCase} is false and {@link Spy} must continue
+	 * searching for the briefcase.
+	 */
 	public void giveCase()
 	{
 		hasCase=true;
 	}
-
+	
+	/**
+	 * This method prints and displays which room has the briefcase.
+	 * @param isDebug
+	 * 		 The isDebug is used if debug mode is executed by the
+	 * 		 user.
+	 * @return when debug mode is executed by user, if {@link Room} has the
+	 * 	   briefcase and {@link #hasCase} is true, then [C] is printed
+	 * 	   over which room has the case. When the debug mode is not used,
+	 * 	   only [R] is printed for all nine rooms and does not show the
+	 * 	   case.	 
+	 */
 	@Override
 	public String toString(boolean isDebug) {
 		if(hasCase&&isDebug)
