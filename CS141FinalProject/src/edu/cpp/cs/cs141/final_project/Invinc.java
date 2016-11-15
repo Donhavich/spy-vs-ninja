b108043db12b884e4127bcbaa262c89d63772bca
Donhavich <gonzalezdodbt@gmail.com>
2016-11-15 09:55:11 -0800

    Update Invinc.java

--- CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Invinc.java
+++ CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Invinc.java
@@ -2,8 +2,8 @@ package edu.cpp.cs.cs141.final_project;
 
 public class Invincibility extends Item {
 		
-	public Invincibility(String itemName) {
-		super(itemName);
+	public Invincibility(String itemName,int x, int y) {
+		super(itemName,x,y);
 	}
 	
 	public void usePotion(Player p) {
