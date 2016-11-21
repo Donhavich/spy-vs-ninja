7d05a90d01349758566dd572b7b1f4e540950271
winghunglau <winghl220@gmail.com>
2016-11-21 14:58:50 -0800

    updated UI

--- CS141FinalProject/src/edu/cpp/cs/cs141/final_project/TextUI.java
+++ CS141FinalProject/src/edu/cpp/cs/cs141/final_project/TextUI.java
@@ -21,7 +21,7 @@ public class TextUI {
 		isDebug = false;
 	}
 	
-	public void start() {
+	public void welcomeMessage() {
 		System.out.println("Game starts.");
 	}
 	
@@ -45,19 +45,19 @@ public class TextUI {
 	}
 	
 	public void spyLives() {
-		System.out.println("Lives: " + ge.lives());
+		System.out.println("Lives: " + ge.getLives());
 	}
 	
 	public void spyBullets() {
-		System.out.println("Bullet: " + ge.bullets());
+		System.out.println("Bullet: " + ge.numOfBullet());
 	}
 	
 	public void spyInvinc() {
-		System.out.println("Invincibility: " + ge.invinc());
+		System.out.println("Invincibility: " + ge.turnsOfInvinc());
 	}
 	
 	public void ninjaInfo() {
-		System.out.println("Number of Ninja: " + ge.numOfNinja());
+		System.out.println("Number of Ninjas: " + ge.numOfNinja());
 	}
 	
 	public void getRadar() {
@@ -80,24 +80,106 @@ public class TextUI {
 		System.out.println("Invalid input. Please input again.");
 	}
 	
-	public void chooseMovement() {
+	public void ninjasAppear() {
+		System.out.println("There is/are ninja(s).");
+	}
+	
+	public void ninjasNotAppear() {
+		System.out.println("There is/are no ninja(s).");
+	}
+	
+	public void zeroBullet() {
+		System.out.println("There is zero bullet.");
+	}
+	
+	public void killedNinjas() {
+		System.out.println("You killed a ninja.");
+	}
+	
+	public void failedToKillNinjas() {
+		System.out.println("You failed to kill a ninja.");
+	}
+	
+	public char chooseMovement() {
 		System.out.println("Choose your movement: [m] Move  [l] Look  [s] Shoot");
 		char c = Input.next().charAt(0);
 		if(c != 'm' || c != 'l' || c != 's') {
 			invalidInput();
 			c = Input.next().charAt(0);
 		}
-		else {
-			ge.playerMove(c);
-		}
+		return c;
 	}
 	
-	public void chooseDirection() {
+	public char chooseDirection() {
 		System.out.println("Choose the direction that you want to move: [w] Up  [a] Left  [s] Down  [d] Right");
 		char c = Input.next().charAt(0);
 		if(c != 'w' || c != 'a' || c != 's' || c != 'd') {
 			invalidInput();
 			c = Input.next().charAt(0);
 		}
+		return c;
 	}	
+	
+	public void StartNewGame() {
+		boolean winning = false;
+		boolean noMove = false;
+		welcomeMessage();
+		ge.createNewGame();
+		chooseMode();
+		while(!ge.isGameOver() || !winning) {
+			boolean isDead = false;
+			ge.resetGrid();
+			while(!isDead || !winning) {
+				char move = chooseMovement();
+				if(move == 'm') {
+					do {
+						char direction = chooseDirection();
+						String action = ge.playerMove(direction);
+						switch(action) {
+						case "noMove": noMove = true;
+							break;
+						case "noCase": emptyRooms();
+							break;
+						case "bullet:": getBullet();
+							break;
+						case "radar": getRadar();
+							break;
+						case "invincible": getInvinc();
+							break;
+						case "getCase": winGame();
+							winning = true;
+							break;
+						}
+					} while(noMove);
+					if(!winning) {
+						isDead = ge.ninjaTurn();
+					}
+				}
+				else if(move == 'l') {
+					char direction = chooseDirection();
+					if(ge.look(direction)) {
+						ninjasAppear();
+					}
+					else {
+						ninjasNotAppear();
+					}
+				}
+				else if(move == 's') {
+					char direction = chooseDirection();
+					String action = ge.shoot(direction);
+					switch(action) {
+					case "noBullet" : zeroBullet();
+						break;
+					case "kill" : killedNinjas();
+						break;
+					case "notKill" : failedToKillNinjas();
+						break;
+					}
+				}
+		}
+		}
+		if(!winning) {
+			loseGame();
+		}
+	}
 }
