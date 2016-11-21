7235c641eaffe64df4e51238a91aba43cf8f4de6
eyMiko <ayymiko@gmail.com>
2016-11-20 22:15:00 -0800

    Update Bullet.java

--- CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Bullet.java
+++ CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Bullet.java
@@ -8,18 +8,39 @@ package edu.cpp.cs.cs141.final_project;
  *
  */
 public class Bullet extends Item {
-	public Bullet(String itemName) {
-		super(itemName);
-	}
-
 	
-
+	private int pointX;
+	private int pointY;
+	private int ammo;
+	
+	public Bullet(String itemName, int pointX, int pointY, int ammo) {
+		super(itemName);
+		this.pointX = pointX;
+		this.pointY = pointY;
+		this.ammo = ammo;
+	}
+	
+	public void setLoc(int locX, int locY) {
+		locX = pointX;
+		locY = pointY;
+	}
+	
+	public int getAmmo() {
+		return ammo;
+	}
+	
+	public void useAmmo(int increaseAmmo) {
+		ammo += increaseAmmo;
+	}
+	
 	@Override
 	public String toString(boolean isDebug) {
 		if(isDebug)
 			return "[b]";
 		else 
 			return "[*]";
-		
+
+	
 	}
+
 }
