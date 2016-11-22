1b38914a70ca939e9c4b41d828bbf8126c140007
lily21 <lynnnguyenquach@gmail.com>
2016-11-22 08:54:55 -0800

    Update DataSave.java

--- CS141FinalProject/src/edu/cpp/cs/cs141/final_project/DataSave.java
+++ CS141FinalProject/src/edu/cpp/cs/cs141/final_project/DataSave.java
@@ -3,10 +3,28 @@
  */
 package edu.cpp.cs.cs141.final_project;
 
+import java.io.*;
 /**
  * @author
  *
  */
-public class DataSave {
-
+public class DataSave implements Serializable {
+	
+	
+	try{
+		FileOutputStream dataSave = new FileOutputStream("filename.dat");
+		
+		ObjectOutputStream save  = new ObjectOutputStream(dataSave);
+		
+		save.writeObject(player)
+		save.writeObject(ninjas);
+		save.writeObject(items);
+		save.writeObject(rooms);
+		
+		
+		save.close();
+	}
+	catch (IO Exception e){
+		e.printStackTrace();
+	}
 }
