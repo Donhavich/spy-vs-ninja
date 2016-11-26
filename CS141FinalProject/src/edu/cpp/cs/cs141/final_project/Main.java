c66aefae0d52a8525da59a0535502d4d3048711a
Xinyuan Wang <wang.x.6f@gmail.com>
2016-11-25 16:45:12 -0800

    main

--- CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Main.java
+++ CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Main.java
@@ -3,7 +3,6 @@
  */
 package edu.cpp.cs.cs141.final_project;
 
-import java.io.IOException;
 
 /**
  * @author 
@@ -13,10 +12,8 @@ public class Main {
 
 	/**
 	 * @param args
-	 * @throws IOException 
-	 * @throws ClassNotFoundException 
 	 */
-	public static void main(String[] args) throws IOException, ClassNotFoundException {
+	public static void main(String[] args) {
 		
 		GameEngine ge=new GameEngine();
 		TextUI TUI=new TextUI(ge);
