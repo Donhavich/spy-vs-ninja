79e9aae91e57699c02f24d5bc1eba68b5d4374ea
eyMiko <ayymiko@gmail.com>
2016-12-01 06:45:31 -0800

    Update Invinc.java

--- CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Invinc.java
+++ CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Invinc.java
@@ -1,5 +1,9 @@
 package edu.cpp.cs.cs141.final_project;
-
+/**
+*
+* Invinc is a subclass of abstract class Item. It reprensents the inviciblity function of the player and gives the 
+* player 5 turns of invincibility.
+/*
 public class Invinc extends Item {
 	
 	
