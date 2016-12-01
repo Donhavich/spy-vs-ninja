f6bc24a16837509415d88fc452ad92d78e6966a3
eyMiko <ayymiko@gmail.com>
2016-12-01 06:42:18 -0800

    Update Radar.java

--- CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Radar.java
+++ CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Radar.java
@@ -5,7 +5,7 @@ package edu.cpp.cs.cs141.final_project;
 
 /**
  * @author
- * Radar is a subclass of the abstract class Item. Radar is an item that reveals the location of the briefcase.
+ * Radar is a subclass of the abstract class Item. Radar is an item that is used by the spy and it reveals the location of the briefcase.
  */
 public class Radar extends Item {
 
