18b45b918668634c7248ea9a804d3c3c5ab5d258
eyMiko <ayymiko@gmail.com>
2016-12-02 15:42:10 -0800

    Update Invinc.java

--- CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Invinc.java
+++ CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Invinc.java
@@ -1,14 +1,38 @@
-package edu.cpp.cs.cs141.final_project;
 /**
-*
-* Invinc is a subclass of abstract class Item. It reprensents the inviciblity function of the player and gives the 
-* player 5 turns of invincibility.
-*/
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodríguez*
+ *
+ * Description of assignment:
+ * 		Create and design a turned based Spy vs Ninja game by using 
+ * 		Object oriented techniques and follows the specifications 
+ * 		based on the rubric. 
+ *
+ * Team: Spirit Coders 
+ * 		Wing Hung Lau
+ * 		Michael Tang
+ * 		Donovan Gonzalez
+ * 		Lynn Nguyen
+ * 		Xinyuan Wang
+ * 		Connor Chase
+ */
+package edu.cpp.cs.cs141.final_project;
+
+/**
+ * 
+ * @author Michael Tang
+ *
+ * The class Invinc is a subclass of abstact class {link @Item}.
+ */
 public class Invinc extends Item {
 	
+/**
+ * This is a super constructor that has the char
+ * of Invincible.
+ */
 	
 	public Invinc() {
 		super('i');
 	}
 	
 }
+
