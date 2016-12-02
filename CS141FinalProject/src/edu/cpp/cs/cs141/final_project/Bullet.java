801f6680a2f20e5516de6217a394e9af0128ddee
eyMiko <ayymiko@gmail.com>
2016-12-01 19:00:17 -0800

    Update Bullet.java

--- CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Bullet.java
+++ CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Bullet.java
@@ -5,7 +5,8 @@ package edu.cpp.cs.cs141.final_project;
 
 /**
  * @author 
- * Bullet is a subclass of Item called Bullet. It represents the "bullet" item of the game and it increases a Spy's ammo count if 0.
+ * The class Bullet is a subclass of the abstract class Item. It represents the "bullet" 
+ * item of the game and it increases a Spy's ammo count if 0.
  */
 public class Bullet extends Item {
 
