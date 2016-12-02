aa781076ad658fe3a97e4d4d0211e679e1c15505
eyMiko <ayymiko@gmail.com>
2016-12-02 15:41:28 -0800

    Update Radar.java

--- CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Radar.java
+++ CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Radar.java
@@ -1,14 +1,33 @@
 /**
- * 
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodríguez
+ *
+ * Description of assignment:
+ * 		Create and design a turned based Spy vs Ninja game by using 
+ * 		Object oriented techniques and follows the specifications 
+ * 		based on the rubric. 
+ *
+ * Team: Spirit Coders 
+ * 		Wing Hung Lau
+ * 		Michael Tang
+ * 		Donovan Gonzalez
+ * 		Lynn Nguyen
+ * 		Xinyuan Wang
+ * 		Connor Chase
  */
 package edu.cpp.cs.cs141.final_project;
 
 /**
- * @author
- * Radar is a subclass of the abstract class Item. Radar is an item that is used by the spy and it reveals the location of the briefcase.
+ * @author Michael Tang
+ *
+ *The class Radar is a subclass of abstract class {link @Item}.
  */
 public class Radar extends Item {
 
+/**
+ * This is a super constructor that has the char
+ * of Radar.
+ */
 	public Radar() {
 		super('r');
 	}
