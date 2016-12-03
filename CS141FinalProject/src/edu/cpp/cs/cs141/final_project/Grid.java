ea94ad036708583fcc19567d4c0b8f5e411d2121
wangx6f <wang.x.6f@gmail.com>
2016-12-02 18:15:18 -0800

    top comment

--- CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Grid.java
+++ CS141FinalProject/src/edu/cpp/cs/cs141/final_project/Grid.java
@@ -1,5 +1,19 @@
 /**
- * 
+ * CS 141: Intro to Programming and Problem Solving
+ * Professor: Edwin Rodríguez*
+ *
+ * Description of assignment:
+ * 		Create and design a turned based Spy vs Ninja game by using 
+ * 		Object oriented techniques and follows the specifications 
+ * 		based on the rubric. 
+ *
+ * Team: Spirit Coders 
+ * 		Wing Hung Lau
+ * 		Michael Tang
+ * 		Donovan Gonzalez
+ * 		Lynn Nguyen
+ * 		Xinyuan Wang
+ * 		Connor Chase
  */
 package edu.cpp.cs.cs141.final_project;
 
